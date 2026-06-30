5e32d7c64e274b02bbfab0802b7cb455cef19e7a
Joonmo You <joonmo.you@gmail.com>
2026-07-01 04:27:18 +0900

    feat: add markdown_to_html_node conversion with block-type handlers

--- src/block_markdown.py
+++ src/block_markdown.py
@@ -1,5 +1,9 @@
 from enum import Enum
 
+from htmlnode import HTMLNode, ParentNode
+from inline_markdown import text_to_textnodes
+from textnode import TextNode, TextType, text_node_to_html_node
+
 
 class BlockType(Enum):
     PARAGRAPH = "paragraph"
@@ -34,4 +38,75 @@ def markdown_to_blocks(markdown):
             lambda block: block != "",
             map(lambda block: block.strip(), markdown.split("\n\n")),
         )
+    )
+
+
+def markdown_to_html_node(markdown: str):
+    blocks = markdown_to_blocks(markdown)
+    for block in blocks:
+        block_to_html_node(block)
+
+
+def block_to_html_node(block: str):
+    block_type = block_to_block_type(block)
+    if block_type == BlockType.PARAGRAPH:
+        pass
+    if block_type == BlockType.HEADING:
+        pass
+    if block_type == BlockType.CODE:
+        pass
+    if block_type == BlockType.QUOTE:
+        pass
+    if block_type == BlockType.UNORDERED_LIST:
+        pass
+    if block_type == BlockType.ORDERED_LIST:
+        pass
+    raise ValueError("invalid block type")
+
+
+def text_to_children(text: str) -> TextNode:
+    return [text_node_to_html_node(text_node) for text_node in text_to_textnodes(text)]
+
+
+def paragraph_to_html_node(block: str) -> ParentNode:
+    lines = block.split("\n")
+    paragraph = " ".join(lines)
+    return ParentNode("p", text_to_children(paragraph))
+
+
+def heading_to_html_node(block: str) -> ParentNode:
+    [heading, text] = block.split(" ", 1)
+    return ParentNode(f"h{len(heading)}", text_to_children(text))
+
+
+def code_to_html_node(block: str) -> ParentNode:
+    if not block.startswith("```") or not block.endswith("```"):
+        raise ValueError("invalid code block")
+    child = text_node_to_html_node(
+        TextNode(block.lstrip("```\n").rstrip("\n```"), TextType.TEXT)
+    )
+    code = ParentNode("code", child)
+    return ParentNode("pre", [code])
+
+
+def quote_to_html_node(block: str) -> ParentNode:
+    lines = block.split("\n")
+    for line in lines:
+        if not line.startswith(">"):
+            raise ValueError("invalid quote block")
+    return ParentNode(
+        "blockquote",
+        text_to_children(" ".join(map(lambda line: line.lstrip(">").strip(), lines))),
+    )
+
+
+def unordered_list_to_html_node(block: str) -> ParentNode:
+    items = block.split("\n")
+    return ParentNode("ul", [ParentNode("li", item.lstrip("- ")) for item in items])
+
+
+def ordered_list_to_html_node(block: str) -> ParentNode:
+    items = block.split("\n")
+    return ParentNode(
+        "ol", [ParentNode("li", item.split(". ", 1)[1]) for item in items]
     )
