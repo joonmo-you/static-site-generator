ffe62fad00b015da0581fd21256d714630c02034
Joonmo You <joonmo.you@gmail.com>
2026-07-09 17:26:20 +0900

    feat: add base_path support for deploying to non-root subdirectories

--- src/main.py
+++ src/main.py
@@ -1,16 +1,19 @@
 import os
+import sys
 import shutil
 
 from copystatic import copy_files
 from gencontent import generate_page, generate_pages_recursive
 
 dir_path_static = "./static"
-dir_path_public = "./public"
+dir_path_public = "./docs"
 dir_path_contents = "./contents"
 template_path = "./template.html"
 
 
 def main():
+    basepath = sys.argv[1] if len(sys.argv) > 1 else "/"
+
     print("Deleting public directory...")
     if os.path.exists(dir_path_public):
         shutil.rmtree(dir_path_public)
@@ -19,7 +22,9 @@ def main():
     copy_files(dir_path_static, dir_path_public)
 
     print("Generating page...")
-    generate_pages_recursive(dir_path_contents, template_path, dir_path_public)
+    generate_pages_recursive(
+        dir_path_contents, template_path, dir_path_public, basepath
+    )
 
 
 if __name__ == "__main__":
